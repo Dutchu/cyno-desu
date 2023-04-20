87e87d419433a86422756b0b9779f4c468563a48
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2023-04-21 00:23:17 +0200

    new modules: bookmark and user. Controller and service class for Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
