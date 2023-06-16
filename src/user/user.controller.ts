a07b9a731fa3de4bd9267b860a4e3bc602d1fb95
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2023-06-16 22:53:24 +0200

    User controller route guard with JWT strategy

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe() {
+    return 'user info';
+  }
+}
