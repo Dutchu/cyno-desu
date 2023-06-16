6d1d3d8534b553b156b48e42a6fc05bc6b58ceff
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2023-06-16 22:53:15 +0200

    User controller route guard with JWT strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     AuthModule,
+    UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
 })
