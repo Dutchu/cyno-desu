223186570286b6b7850d4d80e8529c529fb23dd3
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2023-06-17 21:15:41 +0200

    Http request status change

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import {
   Body,
   Controller,
-  ParseIntPipe,
+  HttpCode,
+  HttpStatus,
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
@@ -19,6 +20,7 @@ export class AuthController {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.singin(dto);
