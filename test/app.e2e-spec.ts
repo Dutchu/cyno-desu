b2de91d953b4ca4d943992d8dd7e678462a928dc
Bartlomiej Blaszczyk <236382@edu.p.lodz.pl>
2023-08-26 16:40:47 +0200

    First test passed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,28 @@
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+
 describe('App e2e', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
   it.todo('should pass');
 });
